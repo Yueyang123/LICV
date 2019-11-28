3e15fa29325bb05b39dc8b42531d1052b5a4267b
Yueyang <you@example.com>
2019-11-28 18:07:00 +0800

    11.28

--- src/main.c
+++ src/main.c
@@ -23,19 +23,52 @@ Log: 11.3 Yueyang
 #include "ColorDec.h"
 #include "Piclib.h"
 
-int main()
+/* Prepare a dummy image. */
+static void FillYuvImage(BYTE* pYuv, int nWidth, int nHeight, int nIndex)
 {
-     Mat dst;
-     Mat_Init;
-     u8* inaddr;
-     int x,y;
-     Mat src1 =Load("..\\picture\\test.bmp","..\\picture\\lena.png");
-     Mat src2 =Load("..\\picture\\test1.bmp","..\\picture\\lena.jpg");
-     save("..\\picture\\test.bmp",&src1);
-     save("..\\picture\\test1.bmp",&src2);
-     Show(&src1);
-     Show(&src2);
-     return 0; 
+	int x, y, i;
+ 
+	i = nIndex;
+ 
+	BYTE* pY = pYuv;
+	BYTE* pU = pYuv + nWidth * nHeight;
+	BYTE* pV = pYuv + nWidth * nHeight * 5 / 4;
+ 
+	/* Y */
+	for (y = 0; y < nHeight; y++)
+	{
+		for (x = 0; x < nWidth; x++)
+		{
+			pY[y * nWidth + x] = x + y + i * 3;
+		}
+	}
+ 
+	/* Cb and Cr */
+	for (y = 0; y < nHeight / 2; y++)
+	{
+		for (x = 0; x < nWidth / 2; x++)
+		{
+			pU[y * (nWidth/2) + x] = 128 + y + i * 2;
+			pV[y * (nWidth/2) + x] = 64 + x + i * 5;
+		}
+	}
 }
+ 
+
+
+// int main()
+// {
+//      Mat dst;
+//      Mat_Init;
+//      u8* inaddr;
+//      int x,y;
+//      Mat src1 =Load("..\\picture\\test.bmp","..\\picture\\lena.png");
+//      Mat src2 =Load("..\\picture\\test1.bmp","..\\picture\\lena.jpg");
+//      save("..\\picture\\test.bmp",&src1);
+//      save("..\\picture\\test1.bmp",&src2);
+//      Show(&src1);
+//      Show(&src2);
+//      return 0; 
+// }
 
  
